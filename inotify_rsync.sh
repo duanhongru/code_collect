0e42266bb4e89620b539413479eb71562aa2b4f4
duanhongru <xingxingduanhongru@163.com>
2016-01-18 16:07:21 +0800

    实际应用同步nginx目录

--- inotify_rsync.sh
+++ inotify_rsync.sh
@@ -2,19 +2,20 @@
 #!/bin/sh
 #date:2013-1-11
 #function:rysnc 10.10.10.20  to  10.10.10.21
-if [ ! -f /etc/21.pas ];then
-        echo "123456">/etc/21.pas
-        /bin/chmod 600 /etc/1.pas
+if [ ! -f /etc/backserver.pas ];then
+        echo "123456">/etc/backserver.pas
+        /bin/chmod 600 /etc/backserver.pas
 fi
-log=/usr/local/inotify/logs/rsync.log
-src="/home/httpd/20dir/"
-host="10.10.10.21"
-module="21dir"
+log=/var/log/rsync.log
+src="/home/duanhongru/nginx-1.9.6/"
+host="61.8.173.158"
+module="nginx-1.9.6"
  
-/usr/local/inotify/bin/inotifywait -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' -e close_write,modify,delete,create,attrib $src |  while read DATE TIME DIR FILE; do
+/usr/bin/inotifywait -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' -e close_write,modify,delete,create,attrib $src |  while read DATE TIME DIR FILE; do
  
        FILECHANGE=${DIR}${FILE}
  
-       /usr/bin/rsync -avH --delete  --progress --password-file=/etc/21.pas $src  --exclude-from="/usr/local/inotify/logs/rules.txt" rsyncuser@$host::$module &
+       /usr/bin/rsync -avH --delete  --progress --password-file=/home/duanhongru/backserver.pas --exclude="objs" $src  duanhongru@$host::$module &
        echo "At ${TIME} on ${DATE}, file $FILECHANGE was backed up via rsync" >> $log
 done
+
